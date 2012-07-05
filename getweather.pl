341b14b131610fd2ecc3a7322c92b96c49c57224
Branden R. Williams <brw@brandolabs.com>
2012-07-05 16:01:38 -0500

    NWS updated their site. We're going to have to do some more fun stuff I think with this as they continue to move old code out of the way.

--- getweather.pl
+++ getweather.pl
@@ -7,8 +7,9 @@ my $forecast = get("http://forecast.weather.gov/MapClick.php?map.x=188&map.y=82&
 my @lines = split(/\n/,$forecast);
 
 my $foundit = "n";
+my $filename = "/var/www/WX/forecast.html";
 
-open (OUTFILE, ">/var/www/WX/forecast.html");
+open (OUTFILE, ">$filename");
 
 foreach (@lines) {
 
@@ -20,21 +21,19 @@ foreach (@lines) {
 		exit;
 	}
 	
-	if ($foundit eq "n" && ($_ =~ /<table width="100%" border="0" align="center" cellpadding="0" cellspacing="0">/)) {
-		$foundit = "y";
+	if ($foundit eq "n" && ($_ =~ /<table width="100%" border="0" align="center" cellpadding="0" cellspacing="0">(.*)<\/table>/)) {
 		print OUTFILE "<table width=\"700\" border=\"0\" align=\"center\" cellpadding=\"0\" cellspacing=\"0\">\n";
-		next;
+		print OUTFILE $1;
+		print OUTFILE "\n</table>\n";
 	}
-
-	if ($foundit eq "y" && !($_ =~ /<\/table>/)) {
-		print OUTFILE "$_\n";
-	}
-
-	if ($foundit eq "y" && ($_ =~ /<\/table>/)) {
-		$foundit = "n";
-		print OUTFILE "$_\n";
-	}
-
 }
 
+close (OUTFILE);
+
+# What we are doing here could probably be done inside perl, but I was
+# lazy and just used SED for this.
+$fixedoutput = `/bin/sed s_/images/_http://forecast.weather.gov/images/_g < /var/www/WX/forecast.html`;
+
+open (OUTFILE, ">$filename");
+print OUTFILE $fixedoutput;
 close (OUTFILE);
